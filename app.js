b564705c5d745ee4584de8cc866a4303bed8fc7d
Mark Kennedy <mark@eurogamer.net>
2013-01-23 15:36:12 +0000

    get rid of some debug stuff

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var fs = require("fs"),
     http = require("http"),
     config = require('config'),
     xregexp = require("xregexp"),
-    dbi = require("node-dbi");
-var redisClient = require("redis");
-console.log( config.redis.host );
-console.log( config.database.host );
+    dbi = require("node-dbi"),
+    redisClient = require("redis"),
+    cronJob = require('cron').CronJob;
+
 var red = redisClient.createClient( config.redis.port, config.redis.host );
-var cronJob = require('cron').CronJob;
 red.on("error", function (err) {
-    console.log("Error " + err);
+    console.error("Error " + err);
 });
+
 dbconf = {
     host: config.database.host,
     user: config.database.user,
@@ -21,56 +21,44 @@ db = new dbi.DBWrapper( 'mysql', dbconf);
 db.connect();
 
 // set up alan!
-
 var alan = fs.readFileSync( "alan.gif" );
 
-// oh you can require json!
 var collections = require( "./collections.json" );
 
-// hacked out of the DBI adapter class
-// why don't they let you access this directly?
 function format( sql, bind ) {
     sql = sql.replace( /\$([1-9]+)/g, function( match, p1, offset, s ) {
-        if (bind.length == 0) {
-            throw new Error('too few parameters given');
-        }
         return db.escape( bind[parseInt(p1)-1] );
     } );
   
     return sql;
 }
 
-new cronJob( '*/10 * * * * *', function() {
+new cronJob( '*/5 * * * * *', function() {
     console.log( "Collecting..." );
     for( var j = 0; j < collections.length; j ++ ) {
         if( collections[j].collection ) {
             red.keys(
                 collections[j].collection.redispattern,
-                (
-                    function( coll, err, keys ) {
-                        for( var ki = 0; ki < keys.length; ki ++ ) {
-                            red.get(
-                                keys[ki],
-                                (
-                                    function( key, coll, err, val ) {
-                                        if( match = new RegExp( coll.pattern ).exec( key ) ) {
-                                            str = format( coll.collection.sql, [val].concat(match.slice(1)) );                 
-                                            //console.log( str );
-                                            db.query( str, function( err, res ) {
-                                                if( err ) {
-                                                    // don't die!
-                                                    console.error( err );
-                                                } else {
-                                                    console.log( "record updated" );
-                                                }
-                                            } );
+                ( function( coll, err, keys ) {
+                    for( var ki = 0; ki < keys.length; ki ++ ) {
+                        red.get(
+                            keys[ki],
+                            ( function( key, coll, err, val ) {
+                                if( match = new RegExp( coll.pattern ).exec( key ) ) {
+                                    str = format( coll.collection.sql, [val].concat(match.slice(1)) );                 
+                                    db.query( str, function( err, res ) {
+                                        if( err ) {
+                                            // don't die!
+                                            console.error( err );
+                                        } else {
+                                            // nothing :)
                                         }
-                                    }
-                                ).bind( null, keys[ki], coll )
-                            );
-                        }
+                                    } );
+                                }
+                            } ).bind( null, keys[ki], coll )
+                        );
                     }
-                ).bind( null, collections[j] )
+                } ).bind( null, collections[j] )
             );
         }
     }
@@ -96,9 +84,9 @@ http.createServer(function(request, response) {
         }
     }
 
-
     response.writeHead(200, { "Content-Type": "image/gif" });
     response.write( alan, "binary" );        
     response.end();
-} ).listen( 3000 );
+} ).listen( config.server.port );
 
+console.log( "Listening on port " + config.server.port + "." );
